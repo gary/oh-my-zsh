88d289fc9d46f6062a219f907c53a5d7ebe05dcb
gary <ge.iams@gmail.com>
2009-09-13 01:58:43 -0400

    color grep only if we're not in emacs

--- grep.zsh
+++ grep.zsh
@@ -2,5 +2,7 @@
 # Color grep results
 # Examples: http://rubyurl.com/ZXv
 #
-export GREP_OPTIONS='--color=auto'
-export GREP_COLOR='1;32'
+if [ -z $EMACS ]; then
+   export GREP_OPTIONS='--color=auto'
+   export GREP_COLOR='1;32'
+fi
