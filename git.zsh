f1b923c8c30b2257bcc8b834b7f1acb0cb6b6d59
gary <ge.iams@gmail.com>
2009-09-13 01:53:14 -0400

    integrated chad humphries' git_prompt_info into the funky theme

--- git.zsh
+++ git.zsh
@@ -1,14 +1,26 @@
 # get the name of the branch we are on
-function git_prompt_info() {
-  if [[ -d .git ]]; then
-    ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-    branch=${ref#refs/heads/}
-    CURRENT_BRANCH="git:(%{$fg[red]%}${branch}%{$fg[blue]%})%{$reset_color%}$(parse_git_dirty)"
-  else
-    CURRENT_BRANCH=''
-  fi
+function git_prompt_info {
+  local ref=$(git symbolic-ref HEAD 2> /dev/null)
+  local gitst="$(git status 2> /dev/null)"
 
-  echo $CURRENT_BRANCH
+  if [[ -f .git/MERGE_HEAD ]]; then
+    if [[ ${gitst} =~ "unmerged" ]]; then
+      gitstatus=" %{$fg[red]%}unmerged%{$reset_color%}"
+    else
+      gitstatus=" %{$fg[green]%}merged%{$reset_color%}"
+    fi
+  elif [[ ${gitst} =~ "Changes to be committed" ]]; then
+    gitstatus=" %{$fg[yellow]%}☂%{$reset_color%}"
+  elif [[ ${gitst} =~ "use \"git add" ]]; then
+    gitstatus=" %{$fg[cyan]%}☁%{$reset_color%}"
+  elif [[ -n `git checkout HEAD 2> /dev/null | grep ahead` ]]; then
+    gitstatus=" %{$fg[yellow]%}☀%{$reset_color%}"
+  else
+    gitstatus=' '
+  fi
+  if [[ -n $ref ]]; then
+    echo " %{$fg_bold[green]%}/${ref#refs/heads/}%{$reset_color%}$gitstatus"
+  fi
 }
 
 parse_git_dirty () {
--- themes/funky.zsh-theme
+++ themes/funky.zsh-theme
@@ -3,12 +3,13 @@
 
 local blue_op="%{$fg[blue]%}[%{$reset_color%}"
 local blue_cp="%{$fg[blue]%}]%{$reset_color%}"
-local path_p="${blue_op}%~${blue_cp}"
+local path_p='${blue_op}%~$(git_prompt_info)${blue_cp}'
 local user_host="${blue_op}%{$fg[yellow]%n@%m${blue_cp}"
 local ret_status="${blue_op}%?${blue_cp}"
 local hist_no="${blue_op}%h${blue_cp}"
 local smiley="%(?,%{$fg[green]%}:%)%{$reset_color%},%{$fg[red]%}:(%{$reset_color%})"
-PROMPT="╭─${path_p}─${user_host}─${ret_status}─${hist_no}
+
+PROMPT="╭─${path_p}─${user_host}─${hist_no}
 ╰─${blue_op}${smiley}${blue_cp} %# "
 local cur_cmd="${blue_op}%_${blue_cp}"
 PROMPT2="${cur_cmd}> "
